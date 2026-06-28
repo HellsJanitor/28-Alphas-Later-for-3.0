2cb9421bd9a4929ee87c0f64e1acfbac3d1a0690
HellsJanitor <147074743+HellsJanitor@users.noreply.github.com>
2026-06-28 18:45:16 +0100

    Main Menu fixes

---
+++ Mods/Alter_ReplaceHardcodedBackground/Harmony/Init.cs
@@ -0,0 +1,44 @@
+﻿using HarmonyLib;
+using System;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+namespace Harmony
+{
+    public class MenuVideoBackground : IModApi
+    {
+        public static Texture2D background;
+        public void InitMod(Mod _modInstance)
+        {
+            Log.Out(" Loading Patch: " + GetType());
+
+            var harmony = new HarmonyLib.Harmony(GetType().ToString());
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            string str = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "/Resources/Backgrounds.unity3d";
+            AssetBundle bundle = AssetBundle.LoadFromFile(str);
+            background = bundle.LoadAsset<Texture2D>("background.png");
+        }
+    }
+    [HarmonyPatch(typeof(MainMenuMono))]
+    [HarmonyPatch("Update")]
+    public class Update
+    {
+        public static bool replaced;
+        public static void Postfix(MainMenuMono __instance)
+        {
+            if (!replaced)
+            {
+                var t = __instance.transform.Find("AnchorCenterCenter/HardcodedLoadingImagery/wdwMainMenuBackground/Texture");
+                if (t)
+                {
+                    var uITexture = t.GetComponent<UITexture>();
+                    if (uITexture)
+                    {
+                        uITexture.mainTexture = MenuVideoBackground.background;
+                        replaced = true;
+                    }
+                }
+            }
+        }
+    }
+}
